930b62195751ebf4603f4f812ef1161972dc8e92
ajitkumar-tech <ajitsanjeevi229@gmail.com>
2026-04-26 19:02:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,15 @@
-body{
-font-family: Arial, sans-serif;
-background:#f0f2f5;
-display:flex;
-justify-content:center;
-align-items:center;
-height:100vh;
+function login(){
+
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+
+if(username=="admin" && password=="admin123")
+{
+document.getElementById("message").innerHTML="Welcome to Mind Circuit 🚀";
+}
+else
+{
+document.getElementById("message").innerHTML="Invalid Username or Password";
 }
 
-.login-container{
-background:white;
-padding:40px;
-width:300px;
-border-radius:8px;
-box-shadow:0 0 10px rgba(0,0,0,0.2);
-text-align:center;
-}
-
-h2{
-margin-bottom:20px;
-}
-
-input{
-width:100%;
-padding:10px;
-margin:10px 0;
-border:1px solid #ccc;
-border-radius:5px;
-}
-
-button{
-width:100%;
-padding:10px;
-background:#1877f2;
-border:none;
-color:white;
-font-size:16px;
-border-radius:5px;
-cursor:pointer;
-}
-
-button:hover{
-background:#145dbf;
-}
-
-#message{
-margin-top:15px;
-font-weight:bold;
 }
